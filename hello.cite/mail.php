7ed9911f56b3e3b3bc6d19245bbfaef92c954da5
oskar41 <jacghbyu@gmail.com>
2017-12-18 10:06:20 +0200

    hello_v1

---
+++ hello.cite/mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$recepient = "createnew.site84@gmail.com";
+$sitename = "Название сайта";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["text"]);
+$message = "Имя: $name \nТелефон: $phone \nemail: $email \nТекст: $text";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
